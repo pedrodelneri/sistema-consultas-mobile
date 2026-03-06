fed3e2b5e63f547f66476083b63cfd60df61c27f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-06 20:51:29 -0300

    primeira versão do app mobile

---
+++ App.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+type Consulta = {
+  id: number;
+  paciente: string;
+  medico: string;
+  data: string;
+  status: "agendada" | "confirmada" | "cancelada" | "realizada";
+};
+export default function App() {
+  const [consulta, setConsulta] = useState<Consulta>({
+    id: 1,
+    paciente: "Carlos Andrade",
+    medico: "Dr. Roberto Silva",
+    data: "28/02/2026",
+    status: "agendada",
+  });
+  function confirmarConsulta() {
+    setConsulta({
+      ...consulta,
+      status: "confirmada",
+    });
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Sistema de Consultas</Text>
+      <View style={styles.card}>
+        <Text>Paciente: {consulta.paciente}</Text>
+        <Text>Médico: {consulta.medico}</Text>
+        <Text>Data: {consulta.data}</Text>
+        <Text>Status: {consulta.status}</Text>
+        {consulta.status === "agendada" && (
+          <view style={styles.botao}>
+          <Button title="Confirmar Consulta" 
+          onPress={confirmarConsulta} />
+          </view>
+        
+        )}
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fafafa",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  card: {
+    width: "80%",
+    padding: 20,
+    borderWidth: 1,
+    borderRadius: 8,
+  },
+  botao: {
+    marginTop: 15
+  }
+});
